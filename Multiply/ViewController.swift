bad1b47fe15af5e912fbaa3dbcf5713396e50271
natalie <njurcik4503@stu.d214.org>
2020-11-16 15:10:19 -0600

    added parts

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    let textField2 = UITextField(frame: CGRect(x: 20, y: 100, width: 100, height: 34))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField2.borderStyle = .roundedRect
+        
+        view.addSubview(textField2)
+        
     }
-
-
+    
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        let number1String = textField1.text!
+        let number2String = textField2.text!
+        
+        let number1 = Int(number1String)!
+        let number2 = Int(number2String)!
+        
+        
+        let answer = number1 * number2
+        
+        outputLabel.text = String(answer)
+    }
+    
 }
 
